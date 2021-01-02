e922df7d4889ddb53e9b5627ce1df23545c386dd
Enekaas <rezaenekaas@gmail.com>
2021-01-02 02:43:31 -0700

    update code

--- upload.js
+++ upload.js
@@ -1,82 +1,110 @@
 // require telegram bot library
-const TL = require("node-telegram-bot-api");
+const TL = require('node-telegram-bot-api')
 
 // keyboards
-const key = require("./src/keys");
+const key = require('./src/keys')
 
 // require database
-require("./src/database");
+require('./src/database')
 
 // require Schemas
-const video = require("./src/models/video");
-const photo = require("./src/models/photo");
-
-// random string func
-const random = () => {
-  return Math.random().toString(36).substr(2, 10);
-};
-
-// force join func
-const isJoin = async (chid, usrid) => {
-  try {
-    let { status } = await bot.getChatMember(chid, usrid);
-    return true ? status !== "left" : false;
-  } catch (e) {
-    return e;
-  }
-};
+const Video = require('./src/models/video')
+const Photo = require('./src/models/photo')
 
 // setup datas
-const username = ""; // => bot username without @
-const token = ""; // => bot token
-const chid = ""; // => channel id
-const chlink = ""; // => channel link
+const admins = []
+const username = '' // => bot username without @
+const token = '' // => bot token
+const chid = '' // => channel id
+const chlink = '' // => channel link
 
 // setup bot
 const bot = new TL(token, {
-  polling: true,
-});
+  polling: true
+})
+
+// random string func
+const random = () => Math.random().toString(36).substr(2, 10)
+
+// force join func
+const isJoin = async (chid, usrid) => {
+  const { status } = await bot.getChatMember(chid, usrid)
+  return true ? status !== 'left' : false
+}
 
 bot.onText(/\/start (.+)/, async (msg, match) => {
   try {
-    let joined = await isJoin(chid, msg.from.id);
+    const joined = await isJoin(chid, msg.from.id)
     if (joined) {
-      photo.find({ id: match[1] }, async (err, data) => {
-        if (err) console.log(err);
-        if (data[0] !== undefined) {
-          let { fileId } = data[0];
-          await bot.sendPhoto(msg.chat.id, fileId);
-        } else {
-          await bot.sendMessage(msg.chat.id, "این فایل وجود ندارد");
-        }
-      });
+      Photo.find({ id: match[1] }, async (err, data) => {
+        Video.find({ id: match[1] }, async (er, vid) => {
+          if (er) console.log(er)
+          if (err) console.log(err)
+          if (data[0]) {
+            const { fileId } = data[0]
+            await bot.sendPhoto(msg.chat.id, fileId)
+          } else if (vid[0]) {
+            const { fileId } = vid[0]
+            await bot.sendVideo(msg.chat.id, fileId)
+          } else {
+            await bot.sendMessage(msg.chat.id, 'این فایل وجود ندارد')
+          }
+        })
+      })
     } else {
       await bot.sendMessage(
         msg.chat.id,
-        "❖لطفا بعد از جوین شدن در کانال های زیر مجددا تلاش کنید",
+        '❖لطفا بعد از جوین شدن در کانال های زیر مجددا تلاش کنید',
         {
           reply_markup: {
-            inline_keyboard: [key.channel("چنل اول", chlink), key.ozviat()],
-          },
+            inline_keyboard: [key.channel('چنل اول', chlink), key.ozviat()]
+          }
         }
-      );
+      )
     }
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-});
+})
 
-bot.on("photo", async (msg) => {
-  let id = random();
-  let fileId =
-    msg.photo[2].file_id || msg.photo[1].file_id || msg.photo[0].file_id;
-  let file = new photo({
-    id,
-    fileId,
-  });
-  file.save(async (err) => {
-    if (err) await bot.sendMessage(msg.chat.id, `به مشکل خوردم:\n${err}`);
-    let link = `https://t.me/${username}?start=${id}`;
-    await bot.sendMessage(msg.chat.id, `فایل شما ذخیره شد:\n${link}`);
-  });
-});
+bot.on('photo', async (msg) => {
+  if (admins.includes(msg.from.id)) {
+    const id = random()
+    const { file_id: fileId } = msg.photo[2] || msg.photo[1] || msg.photo[0]
+    const File = new Photo({
+      id,
+      fileId
+    })
+    File.save(async (err) => {
+      if (err) await bot.sendMessage(msg.chat.id, `به مشکل خوردم:\n${err}`)
+      const link = `https://t.me/${username}?start=${id}`
+      await bot.sendMessage(msg.chat.id, `فایل شما ذخیره شد:\n${link}`)
+    })
+  }
+})
+
+bot.on('video', async (msg) => {
+  if (admins.includes(msg.from.id)) {
+    const id = random()
+    const { file_id: fileId } = msg.video
+    const File = new Video({
+      id,
+      fileId
+    })
+    File.save(async (err) => {
+      if (err) await bot.sendMessage(msg.chat.id, `به مشکل خوردم:\n${err}`)
+      const link = `https://t.me/${username}?start=${id}`
+      await bot.sendMessage(msg.chat.id, `فایل شما ذخیره شد:\n${link}`)
+    })
+  }
+})
+
+bot.onText(/\/panel/, async (msg) => {
+  try {
+    if (admins.includes(msg.from.id)) {
+      await bot.sendMessage(msg.chat.id, 'به پنل مدیریت خوش اومدی', key.admin())
+    }
+  } catch (e) {
+    console.log(e.message)
+  }
+})
